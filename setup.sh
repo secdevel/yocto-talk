254b628fff0d0ba41da9c8254e06cf3e72f0ff41
secdevel <securedeveloper@protonmail.com>
2025-11-09 16:31:59 +0000

    corrected path

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ set -e
 # Source the environment
 source poky/oe-init-build-env build
 
-cp conf/local.conf.sample build/conf/local.conf
-cp conf/bblayers.conf.sample build/conf/bblayers.conf
+cp ../conf/local.conf.sample conf/local.conf
+cp ../conf/bblayers.conf.sample conf/bblayers.conf
